c1aee6de0d59e1be81a60819cb30d9e6f5518bc3
kazu-08 <syame.ken3@gmail.com>
2024-10-13 10:42:11 +0900

    path5

--- assets/src/components/Header.jsx
+++ assets/src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
     return(
         <header className="header">
             <h1>Birdwatching</h1>
-            <img src="./dove.png" alt="a simple dove logo"/>
+            <img src="dove.png" alt="a simple dove logo"/>
         </header>
     );
 }

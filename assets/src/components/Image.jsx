c7c5058abac595f45f14c21c90fa78f5d2f12462
kazu-08 <syame.ken3@gmail.com>
2024-10-13 10:45:12 +0900

    path8

--- assets/src/components/Image.jsx
+++ assets/src/components/Image.jsx
@@ -3,10 +3,10 @@ import Image from "./Image.jsx";
 export default function image() {
     return (
         <image className="image">
-            <a href="/assets/src/components/favorite-1.jpg"><img src="./favorite-1_th.jpg" alt="Small black bird, black claws, long black slender beak, links to larger version of the image"/></a>
-            <a href="/assets/src/components/favorite-2.jpg"><img src="./favorite-2_th.jpg" alt="Top half of a pretty bird with bright blue plumage on neck, light colored beak, blue headdress, links to larger version of the image"/></a>
-            <a href="/assets/src/components/favorite-3.jpg"><img src="./favorite-3_th.jpg" alt="Top half of a large bird with white plumage, very long curved narrow light colored break, links to larger version of the image"/></a>
-            <a href="/assets/src/components/favorite-4.jpg"><img src="./favorite-4_th.jpg" alt="Large bird, mostly white plumage with black plumage on back and rear, long straight white beak, links to larger version of the image"/></a>
+            <a href="/assets/src/components/favorite-1.jpg"><img src="/assets/src/components/favorite-1_th.jpg" alt="Small black bird, black claws, long black slender beak, links to larger version of the image"/></a>
+            <a href="/assets/src/components/favorite-2.jpg"><img src="/assets/src/components/favorite-2_th.jpg" alt="Top half of a pretty bird with bright blue plumage on neck, light colored beak, blue headdress, links to larger version of the image"/></a>
+            <a href="/assets/src/components/favorite-3.jpg"><img src="/assets/src/components/favorite-3_th.jpg" alt="Top half of a large bird with white plumage, very long curved narrow light colored break, links to larger version of the image"/></a>
+            <a href="/assets/src/components/favorite-4.jpg"><img src="/assets/src/components/favorite-4_th.jpg" alt="Large bird, mostly white plumage with black plumage on back and rear, long straight white beak, links to larger version of the image"/></a>
 
         </image>
     );
